c71c19b0556ee6e97ccf5006bede8440d9f73751
Leonardo Souza <143288413+Leo-gsouza@users.noreply.github.com>
2024-05-31 13:59:06 -0300

    atualização curiosidades

--- scripts/paginas2.js
+++ scripts/paginas2.js
@@ -15,21 +15,26 @@ function aba(aba_on, cont_on, aba_off1, cont_off1, aba_off2, cont_off2, aba_off3
     aba_on.style.background = 'darkred'
     aba_on.style.boxShadow ='0px 0px 10px white'
     aba_on.style.scale = '1.1'
+    aba_on.style.borderRadius = '5px'
+    aba_on.style.border = '1px solid white'
     cont_on.style.display = 'block'
     
     aba_off1.style.background = 'none'
     aba_off1.style.boxShadow ='none'
     aba_off1.style.scale = 'none'
+    aba_off1.style.border = 'none'
     cont_off1.style.display ='none'
 
     aba_off2.style.background = 'none'
     aba_off2.style.boxShadow ='none'
     aba_off2.style.scale = 'none'
+    aba_off2.style.border = 'none'
     cont_off2.style.display ='none'
 
     aba_off3.style.background = 'none'
     aba_off3.style.boxShadow ='none'
     aba_off3.style.scale = 'none'
+    aba_off3.style.border = 'none'
     cont_off3.style.display ='none'
    
 }
