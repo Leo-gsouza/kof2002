6ed55a0ecf647c4a8e46ef699691574696a66387
Leonardo Souza <143288413+Leo-gsouza@users.noreply.github.com>
2024-05-30 22:22:49 -0300

    pagina curiosidades

--- scripts/paginas.js
+++ scripts/paginas.js
@@ -1,10 +1,12 @@
 var abaHistoria = document.getElementById('aba-um')
 var abaGolpes = document.getElementById('aba-dois')
 var abaCombates = document.getElementById('aba-tres')
+var abaCuriosidades = document.getElementById('aba-quatro')
 
 var conteudoHistoria = document.getElementById('conteudo-um')
 var conteudoGolpes = document.getElementById('conteudo-dois')
 var conteudoCombates = document.getElementById('conteudo-tres')
+var conteudoCuriosidades = document.getElementById('conteudo-quatro')
 
 
 function historia(){
@@ -22,6 +24,11 @@ function historia(){
     abaCombates.style.boxShadow ='none'
     abaCombates.style.scale = 'none'
     conteudoCombates.style.display ='none'
+
+    abaCuriosidades.style.background = 'none'
+    abaCuriosidades.style.boxShadow = 'none'
+    abaCuriosidades.style.scale = 'none'
+    conteudoCuriosidades.style.display ='none'
 }
 
 function golpes(){
@@ -39,6 +46,11 @@ function golpes(){
     abaCombates.style.boxShadow ='none'
     abaCombates.style.scale = 'none'
     conteudoCombates.style.display ='none'
+
+    abaCuriosidades.style.background = 'none'
+    abaCuriosidades.style.boxShadow = 'none'
+    abaCuriosidades.style.scale = 'none'
+    conteudoCuriosidades.style.display ='none'
 }
 
 function combates(){
@@ -56,5 +68,32 @@ function combates(){
     abaGolpes.style.boxShadow ='none'
     abaGolpes.style.scale = 'none'
     conteudoGolpes.style.display ='none'
+
+    abaCuriosidades.style.background = 'none'
+    abaCuriosidades.style.boxShadow = 'none'
+    abaCuriosidades.style.scale = 'none'
+    conteudoCuriosidades.style.display ='none'
+}
+
+function curiosidades(){
+    abaCuriosidades.style.background ='darkred'
+    abaCuriosidades.style.boxShadow = '0px 0px 10px white'
+    abaCuriosidades.style.scale = '1.2'
+    conteudoCuriosidades.style.display = 'block'
+
+    abaHistoria.style.background = 'none'
+    abaHistoria.style.boxShadow ='none'
+    abaHistoria.style.scale = 'none'
+    conteudoHistoria.style.display ='none'
+
+    abaGolpes.style.background = 'none'
+    abaGolpes.style.boxShadow ='none'
+    abaGolpes.style.scale = 'none'
+    conteudoGolpes.style.display ='none'
+
+    abaCombates.style.background = 'none'
+    abaCombates.style.boxShadow ='none'
+    abaCombates.style.scale = 'none'
+    conteudoCombates.style.display ='none'
 }
 
---
+++ scripts/paginas2.js
@@ -0,0 +1,36 @@
+
+var h = document.getElementById('aba-um')
+var g = document.getElementById('aba-dois')
+var d = document.getElementById('aba-tres')
+var c = document.getElementById('aba-quatro')
+
+var ch = document.getElementById('conteudo-um')
+var cg = document.getElementById('conteudo-dois')
+var cd = document.getElementById('conteudo-tres')
+var cc = document.getElementById('conteudo-quatro')
+
+
+function aba(aba_on, cont_on, aba_off1, cont_off1, aba_off2, cont_off2, aba_off3, cont_off3){
+
+    aba_on.style.background = 'darkred'
+    aba_on.style.boxShadow ='0px 0px 10px white'
+    aba_on.style.scale = '1.1'
+    cont_on.style.display = 'block'
+    
+    aba_off1.style.background = 'none'
+    aba_off1.style.boxShadow ='none'
+    aba_off1.style.scale = 'none'
+    cont_off1.style.display ='none'
+
+    aba_off2.style.background = 'none'
+    aba_off2.style.boxShadow ='none'
+    aba_off2.style.scale = 'none'
+    cont_off2.style.display ='none'
+
+    aba_off3.style.background = 'none'
+    aba_off3.style.boxShadow ='none'
+    aba_off3.style.scale = 'none'
+    cont_off3.style.display ='none'
+   
+}
+
