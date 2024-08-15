e8019bb7a10514d1396d7491f8b275b8520a3fc3
Leonardo Souza <143288413+Leo-gsouza@users.noreply.github.com>
2024-08-15 12:16:09 -0300

    kyo concluido

--- scripts/paginas2.js
+++ scripts/paginas2.js
@@ -3,12 +3,13 @@ function aba( botao, conteudo, desativados, fechadas){
     if (conteudo.style.display == 'block'){
         conteudo.style.display = 'none'
         botao.style.background = '#B2BEBF'
+        botao.style.scale = '1'
        
 
     }else{
         conteudo.style.display = 'block'
         botao.style.background = 'darkred'
-       
+        botao.style.scale = '1.1'
 
         fechadas.forEach(fechada => {
             fechada.style.display = 'none'
