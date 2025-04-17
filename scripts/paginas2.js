27379f684286796973322d5fe206dade3d0ded3a
Leonardo Souza <143288413+Leo-gsouza@users.noreply.github.com>
2025-04-17 01:18:48 -0300

    Ajuste no mediaquery 1920
    Ajustei os botoes

--- scripts/paginas2.js
+++ scripts/paginas2.js
@@ -4,12 +4,14 @@ document.getElementById("voltar").addEventListener("click", ()=> {
 })
 
 function aba( botao, conteudo, desativados, fechadas){
+    
     if (conteudo.style.display == 'flex'){
         conteudo.style.display = 'flex'
 
     }else{
         conteudo.style.display = 'flex'
         botao.style.scale = '1.1'
+        botao.style.backgroundColor = 'var(--cor0)'
         botao.classList.add('glow')
 
         }
@@ -20,9 +22,29 @@ function aba( botao, conteudo, desativados, fechadas){
 
         desativados.forEach(desativado =>{
             desativado.style.scale = '1'
+            desativado.style.backgroundColor = 'var(--cor2)'
             desativado.classList.remove('glow')
         })
     }
+
+var golpe = document.getElementById('golpes2')
+
+let ampliado = false
+
+/* golpe.addEventListener("click", () => {
+    if(!ampliado){
+        golpe.style.transform = 'scale(1.2)'
+        golpe.style.boxShadow = '0px 0px 100px black'
+        golpe.style.background = 'white'
+
+    }else{
+        golpe.style.transform = 'scale(1)'
+        golpe.style.boxShadow = 'none'
+        golpe.style.background = 'transparent'
+    }
+    ampliado = !ampliado
+
+}) */
    
 
 
